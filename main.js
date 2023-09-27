77c91b75aa14adb30e64c932df5b0a639fc347f4
IsmoilovAmirbek <ismoilovamirbek3@gmail.com>
2023-09-27 22:28:50 +0500

    New Registor

---
+++ main.js
@@ -0,0 +1,16 @@
+let CARD = document.querySelector("#CARD")
+let ism = document.querySelector("#ism")
+let Familiya = document.querySelector("#Familiya")
+let number = document.querySelector("#number")
+let buttom = document.querySelector("button")
+let a = document.querySelector("a")
+
+
+buttom.onclick = () => {
+alert("Thank you for your purchase :)")
+}
+
+
+a.onclick = () => {
+   
+}
